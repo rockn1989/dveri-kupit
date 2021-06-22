162646a89e367a61b47e91e61160c33f3855d3a8
rockn1989 <gold_100@bk.ru>
2021-06-22 18:35:14 +0300

    add product slider

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -26,5 +26,50 @@ $(function () {
     slidesToShow: 1,
     slidesToScroll: 1,
     lazyLoad: "ondemand",
+    responsive: [
+      {
+        breakpoint: 959,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
+
+  /**
+   * Products slider
+   */
+  const $productSlider = $(".product-slider .slider");
+
+  $productSlider.slick({
+    infinite: false,
+    arrows: true,
+    dots: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    variableWidth: true,
+    fade: false,
+    cssEase: "linear",
+    swipeToSlide: true,
+    prevArrow:
+      '<div class="btn-slide slick-prev"><svg width="58px" height="14px" class="arrow-left"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/icon-svg/sprite/sprite.svg#arrow-left"></use></svg></i></div>',
+    nextArrow:
+      '<div class="btn-slide slick-next"><svg width="58px" height="14px" class="arrow-right"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/icon-svg/sprite/sprite.svg#arrow-right"></use></svg></div>',
+    responsive: [
+      {
+        breakpoint: 959,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+        },
+      },
+    ],
   });
 });
