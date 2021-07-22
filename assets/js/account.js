9a638fe1a96fedd182814a663004250076565b7d
rockn1989 <gold_100@bk.ru>
2021-07-22 17:30:48 +0300

    add account order page

--- assets/js/account.js
+++ assets/js/account.js
@@ -8,4 +8,15 @@ $(function () {
       .stop(true, true)
       .slideToggle("350");
   });
+
+  $(".js__toggle-account-order-list").on("click", function (e) {
+    e.preventDefault();
+
+    $(this)
+      .toggleClass("open")
+      .parents(".account-order-list__item")
+      .find(".account-order-list__orders")
+      .stop(true, true)
+      .slideToggle("350");
+  });
 });
